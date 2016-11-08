1cea92bdacd7f9069e199c668f4b02f2a3c76a33
Chris Umbel <chris@chrisumbel.com>
2016-11-08 08:17:22 -0500

    go

--- src/ConcourseTest/TestController.cs
+++ src/ConcourseTest/TestController.cs
@@ -5,9 +5,20 @@ namespace ConcourseTest
 	[Route("/")]
     public class TestController : Controller 
     {
+        private AppDbContext context;
+
+        public TestController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
 		[HttpGet]
 		public virtual IActionResult Index() 
 		{
+            Thing thing = new Thing() {ID = 1, Name = "Phone"};
+            this.context.Add(thing);
+            this.context.SaveChanges();
+                        
             return this.Ok("OK");
         }        
     }
