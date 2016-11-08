a27f3b8df18eedbac7f8f7bc1c411c29a0d61623
Chris Umbel <chris@chrisumbel.com>
2016-11-08 12:25:46 -0500

    packages

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM microsoft/dotnet:1.0.0-preview2-sdk
 WORKDIR /app
 COPY . ./
 WORKDIR src/ConcourseTest
-RUN dotnet restore
-RUN dotnet build
+RUN export NUGET_PACKAGES=../../packages 
+    dotnet restore
+    dotnet build
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 cd /app/src/ConcourseTest
 sleep 4
 Data__DefaultConnection__ConnectionString="Server=db;Port=5432;User Id=postgres;Password=postgres" dotnet ef database update
-dotnet run --server.urls=http://0.0.0.0:5000
+NUGET_PACKAGES=../../packages dotnet run --server.urls=http://0.0.0.0:5000
